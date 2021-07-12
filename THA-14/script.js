1af5ead26d8cb336615eb110162a2a64ad52ad96
swetanjalisapra <49619848+swetanjalisapra@users.noreply.github.com>
2021-07-13 01:21:39 +0530

    updated

---
+++ THA-14/script.js
@@ -0,0 +1,27 @@
+function onClick() {
+  console.log("I am Clicked");
+}
+
+function ondblClick() {
+  console.log("Double Clicked");
+}
+
+function drag() {
+  console.log("Dragged");
+}
+
+function keyDwn() {
+  console.log("Key was down");
+}
+function keyUp() {
+  console.log("Key was up");
+}
+function mouseDw() {
+  console.log("Mouse Down");
+}
+function mseMove() {
+  console.log("Mouse Moved");
+}
+function mseOver() {
+  console.log("Mouse Over");
+}
